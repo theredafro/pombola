6498fc73b1d55c35a2cecf354d7ad054b5072d65
Chris Mytton <chrismytton@gmail.com>
2016-02-23 19:28:41 +0100

    Remove PU search logic from Nigeria views
    This isn't fully working at the moment as I've just commented out the
    bit which looks up the parent views, but it's not too far off.

--- pombola/nigeria/views.py
+++ pombola/nigeria/views.py
@@ -1,5 +1,7 @@
 import re
 
+import requests
+
 from mapit.models import Area
 
 from pombola.core.models import Place
@@ -28,100 +30,6 @@ class NGHomeView(HomeView):
             context['editable_content'] = None
         return context
 
-# These are hardcoded here rather than being introduced into the database to
-# avoid having a huge number of duplicated codes in MapIt. As it is largely a
-# presentation thing though I don't think it is too big an issue.
-state_number_to_letter_mappings = {
-    "1": "AB",
-    "2": "AD",
-    "3": "AK",
-    "4": "AN",
-    "5": "BA",
-    "6": "BY",
-    "7": "BE",
-    "8": "BO",
-    "9": "CR",
-    "10": "DE",
-    "11": "EB",
-    "12": "ED",
-    "13": "EK",
-    "14": "EN",
-    "15": "GO",
-    "16": "IM",
-    "17": "JI",
-    "18": "KD",
-    "19": "KN",
-    "20": "KT",
-    "21": "KE",
-    "22": "KO",
-    "23": "KW",
-    "24": "LA",
-    "25": "NA",
-    "26": "NI",
-    "27": "OG",
-    "28": "ON",
-    "29": "OS",
-    "30": "OY",
-    "31": "PL",
-    "32": "RI",
-    "33": "SO",
-    "34": "TA",
-    "35": "YO",
-    "36": "ZA",
-    "37": "FC",
-}
-
-def tidy_up_pun(pun):
-    """
-    Tidy up the query into something that looks like PUNs we are expecting
-
-    # None returns empty string
-    >>> tidy_up_pun(None)
-    ''
-
-    # Tidy up and strip as expected
-    >>> tidy_up_pun("AB:01:23:45")
-    'AB:1:23:45'
-    >>> tidy_up_pun("AB--01::23 45")
-    'AB:1:23:45'
-    >>> tidy_up_pun("  AB--01::23 45  ")
-    'AB:1:23:45'
-
-    # Convert state numbers to state code, if found
-    >>> tidy_up_pun("01:01:23:45")
-    'AB:1:23:45'
-    >>> tidy_up_pun("01")
-    'AB'
-    >>> tidy_up_pun("99:01:23:45")
-    '99:1:23:45'
-    """
-
-    if not pun:
-        pun = ""
-
-    pun = pun.strip().upper()
-    pun = re.sub(r'[^A-Z\d]+', ':', pun ) # separators to ':'
-    pun = re.sub(r'^0+', '',  pun ) # trim leading zeros at start of string
-    pun = re.sub(r':0+', ':', pun ) # trim leading zeros for each component
-
-    # PUNs starting with a number shoud be converted to start with a state code
-    if re.match(r'^\d', pun):
-        state_number = pun.split(':')[0]
-        state_code = state_number_to_letter_mappings.get(state_number, state_number)
-        pun = re.sub(r'^' + state_number, state_code, pun)
-
-    return pun
-
-# A regular expression to match any PUN after it's been tidied
-pun_re = re.compile('''
-    ^(
-       [A-Z]{2}|
-       [A-Z]{2}:\d+|
-       [A-Z]{2}:\d+:\d+|
-       [A-Z]{2}:\d+:\d+:\d+
-    )$
-''', re.VERBOSE)
-
 
 class NGSearchView(SearchBaseView):
 
@@ -138,7 +46,7 @@ class NGSearchView(SearchBaseView):
             return context
 
         # So now we know that the query is a PUN:
-        query = tidy_up_pun(self.request.GET.get('q'))
+        query = self.request.GET.get('q')
         context['raw_query'] = query
         context['query'] = query
         context['area'] = self.get_area_from_pun(query)
@@ -148,8 +56,8 @@ class NGSearchView(SearchBaseView):
             area = context['area']
 
             # Get the area PUN and name to store in context for template
-            context['area_pun_code'] = area.codes.filter(type__code="poll_unit")[0].code
-            context['area_pun_name'] = area.names.filter(type__code="poll_unit")[0].name
+            context['area_pun_code'] = area['codes']['poll_unit']
+            context['area_pun_name'] = area['name']
 
             # Get the place object for the containing state
             context['state'] = self.get_state(
@@ -164,29 +72,24 @@ class NGSearchView(SearchBaseView):
             context['governor'] = self.find_governor(context['state'])
 
             # work out the polygons to match to, may need to go up tree to parents.
-            area_for_polygons = self.find_containing_area(area)
-            if area_for_polygons:
-                area_polygons = area_for_polygons.polygons.collect()
+            # area_for_polygons = self.find_containing_area(area)
+            # if area_for_polygons:
+            #     area_polygons = area_for_polygons.polygons.collect()
 
-                context['federal_constituencies'] = self.get_district_data(
-                    self.find_matching_places("FED", area_polygons),
-                    "representative"
-                )
+            #     context['federal_constituencies'] = self.get_district_data(
+            #         self.find_matching_places("FED", area_polygons),
+            #         "representative"
+            #     )
 
-                context['senatorial_districts']  = self.get_district_data(
-                    self.find_matching_places("SEN", area_polygons),
-                    "senator"
-                )
+            #     context['senatorial_districts']  = self.get_district_data(
+            #         self.find_matching_places("SEN", area_polygons),
+            #         "senator"
+            #     )
         return context
 
     def parse_params(self):
         super(NGSearchView, self).parse_params()
-        tidied_as_if_pun = tidy_up_pun(self.query)
-        # Check if this is a known form of PUN:
-        if pun_re.search(tidied_as_if_pun):
-            self.pun = tidied_as_if_pun
-        else:
-            self.pun = None
+        self.pun = self.query
 
     def get(self, request, *args, **kwargs):
         query = self.request.GET.get('q')
@@ -239,7 +142,7 @@ class NGSearchView(SearchBaseView):
 
     def convert_area_to_place(self, area):
         try:
-            return Place.objects.get(mapit_area=area)
+            return Place.objects.get(mapit_area_id=area['id'])
         except Place.DoesNotExist:
             return None
 
@@ -252,14 +155,7 @@ class NGSearchView(SearchBaseView):
         forgiving in case of input error.
         """
 
-        while pun:
-            try:
-                area = Area.objects.get(codes__code=pun)
-                return area
-            except Area.DoesNotExist:
-                # strip off last component
-                pun = re.sub(r'[^:]+$', '', pun)
-                pun = re.sub(r':$', '', pun)
+        return requests.get('https://pu-lookup.herokuapp.com/lookup/{}'.format(pun)).json()
 
     def get_state(self, matched_pun, state_code, area):
         if ":" in matched_pun:
